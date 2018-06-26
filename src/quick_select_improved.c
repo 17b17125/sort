ed67fa494d7f878fd63b400f71dd32a72d361ac0
Sasagawa Takuya <sasagawa.t.ae@is-mac10.edu.c.titech.ac.jp>
2018-06-26 16:44:26 +0900

    実験の前に

--- src/quick_select_improved.c
+++ src/quick_select_improved.c
@@ -10,22 +10,25 @@ A[0], A[1], ..., A[n-1] の中でk+1番目に小さい値を返す関数
 ただし、Aの中身は書き換えてしまう。
 */
 int quick_select(int A[], int n, int k){
-  int i, j, pivot;
+  int i, j, l, pivot;
 
 // 先頭の要素をピボットとする
   pivot = A[0];
-  for(i = j = 1; i < n; i++){
+  for(i = j = l = 1; i < n; i++){
     if(A[i] <= pivot){
-      int z = A[j];
-      A[j] = A[i];
+      int z = A[j+l-1];
+      A[j+l-1] = A[i];
       A[i] = z;
       j++;
+      if(A[i] == pivot){
+        l++;
+      }
     }
   }
 
   if(j == k+1) return pivot;
   else if(j < k+1) return quick_select(A+j, n-j, k-j);
-  else return quick_select(A+1, j-1, k);
+  else return quick_select(A+l, j-l, k);
 }
 
 int main(){
