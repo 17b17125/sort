a450a3871ae0b7d4bc6011d04d86d68be9c6f52c
Sasagawa Takuya <sasagawa.t.ae@is-mac10.edu.c.titech.ac.jp>
2018-06-26 17:17:40 +0900

    Improved

--- src/quick_sort_improved.c
+++ src/quick_sort_improved.c
@@ -9,6 +9,30 @@ int A[N];
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+  int i, j, l, pivot;
+
+  if(n<=0) return;
+// 先頭の要素をピボットとする
+  pivot = A[0];
+  for(i = j = l = 1; i < n; i++){
+    if(A[i] <= pivot){
+      int z = A[j+l-1];
+      A[j+l-1] = A[i];
+      A[i] = z;
+      j++;
+      if(A[i] == pivot){
+        l++;
+      }
+    }
+  }
+  for(i = 0;i < l; i++){
+    int z = A[i];
+    A[i] = A[j-i-1];
+    A[j-i-1]=z;
+  }
+  quick_sort(A, j-l);
+  quick_sort(A+j, n-j);
+  return;
 }
 
 int main(){
